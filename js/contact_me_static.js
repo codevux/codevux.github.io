bb4a8b76c6b55fd7ad22ad0ce523383fc9b290d7
Andre <killuahxhass@gmail.com>
2020-05-09 21:18:40 -0300

    Initial commit

---
+++ js/contact_me_static.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+// $("#submitFormBtn").click(function(){        
+//     $("#contactForm").submit(function(e) {
+//         return false;
+// });                  
